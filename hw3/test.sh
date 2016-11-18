27931f1ae8691f6920c9fce1bf650c3220b6df1f
Zong-Ze <r05921012@ntu.edu.tw>
2016-11-18 18:07:32 +0800

    rm hw3

--- hw3/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-THEANO_FLAGS=device=gpu,floatX=float32 python self_training_test.py $1 $2 $3
--- hw3/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-THEANO_FLAGS=device=gpu,floatX=float32 python self_training.py $1 $2
